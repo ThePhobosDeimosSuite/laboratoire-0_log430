e70e935b9a3df3aea0ea4094611d3a3e814b4e50
Pierre-Émile Brassard <pierreemilebrassard@gmail.com>
2025-06-04 14:47:08 +0000

    move stuff to controller

--- src/view/manager/dashboard-view.ts
+++ src/view/manager/dashboard-view.ts
@@ -5,70 +5,26 @@ import mainBusinessView from "./main-business-view.js"
 import terminalKit from "terminal-kit";
 const { terminal } = terminalKit
 
-interface StocksPerStore {
-    storeId:number, 
-    lowStockProduct: 
-    {
-        name:string, 
-        amount:number
-    }[]
-}
 
 export default async () => {
     terminal.clear()
 
-    const allSalesPerStore = {}
-
-    for(const storeId of appConst.storeShopId) {
-        const sales = await Manager.searchSales(undefined, storeId)
-        const totalSalesPrice = sales.reduce((partialsum, a) => partialsum + a.totalPrice, 0)
-        allSalesPerStore[`Store ${storeId}`] = totalSalesPrice + " $"
-    }
+    const dashboardView = await Manager.getDashboardView()
 
     terminal.cyan("TOTAL SALES PER STORE: ")
-    colorizeJSON(allSalesPerStore)
+    colorizeJSON(dashboardView.allSalesPerStore)
 
     terminal.once('key', async () => {
-        const lowStocksPerStore: StocksPerStore[] = []
-
-        for (const storeId of appConst.storeShopId) {
-            const allProduct = await Manager.searchProduct(undefined, undefined, undefined, storeId)
-            const lowStockProduct = allProduct
-                .filter(p => p.stock.length == 0 || p.stock[0]?.amount <= 5)
-                .map(p => ({ name: p.name, amount: p.stock[0]?.amount ?? 0 }))
-            if (lowStockProduct.length > 0) {
-                lowStocksPerStore.push({
-                    storeId,
-                    lowStockProduct
-                })
-            }
-        }
-
         terminal.clear()
+
         terminal.red("LOW STOCK (below 5): \n")
-        colorizeJSON(lowStocksPerStore)
+        colorizeJSON(dashboardView.lowStocksPerStore)
 
         terminal.once('key', async () => {
-
-            const highStocksPerStore: StocksPerStore[] = []
-
-            for (const storeId of appConst.storeShopId) {
-                const allProduct = await Manager.searchProduct(undefined, undefined, undefined, storeId)
-                const highStockProduct = allProduct
-                    .filter(p => p.stock.length > 0 && p.stock[0].amount >= 100)
-                    .map(p => ({ name: p.name, amount: p.stock[0]?.amount ?? 0 }))
-                if(highStockProduct.length > 0) {
-                    highStocksPerStore.push({
-                        storeId,
-                        lowStockProduct: highStockProduct
-                    })
-                }
-            }
-
             terminal.clear()
 
             terminal.green("HIGH STOCK (above 100): \n")
-            colorizeJSON(highStocksPerStore)
+            colorizeJSON(dashboardView.highStocksPerStore)
 
             terminal.once('key', async () => {
                 mainBusinessView()
--- src/view/manager/sales-report-view.ts
+++ src/view/manager/sales-report-view.ts
@@ -15,66 +15,32 @@ export default async () => {
         if (response.canceled) {
             mainBusinessView()
         } else {
-            // List of all sales 
             const selectedStoreId = appConst.storeShopId[response.selectedIndex]
-            terminal.clear()
-            const sales = await Manager.searchSales(undefined, selectedStoreId)
+            const salesReport = await Manager.getSalesReport(selectedStoreId)
 
-            const formatedSales = sales.map(s => ({
-                isCancelled: s.isCancelled,
-                salesPrice: s.totalPrice,
-                productSold: s.productSales.map(p => ({
-                    name: p.product.name,
-                    price: p.product.price,
-                    amount: p.amount
-                }))
-            }))
+            terminal.clear()
 
             terminal.cyan(`LIST OF ALL SALES FOR STORE (${selectedStoreId})\n`)
-            colorizeJSON(formatedSales)
+            colorizeJSON(salesReport.sales)
             
             terminal.once('key', () => {
-                // Total of all sales
-                const totalSalesPrice = sales.reduce((partialsum, a) => partialsum + a.totalPrice, 0)
 
                 terminal.clear()
-                terminal.cyan(`TOTAL SALES PRICE FOR (Store ${selectedStoreId}): ${totalSalesPrice} $`)
+
+                terminal.cyan(`TOTAL SALES PRICE FOR (Store ${selectedStoreId}): ${salesReport.totalSalesPrice} $`)
 
                 terminal.once('key', () => {
-                    // Total time a product has been sold
+
                     terminal.clear()
-                    const groupedProductSales = {}
-
-                    for(const sale of sales) {
-                        if(sale.isCancelled) continue
-
-                        for (const productSale of sale.productSales) {
-                            const { name } = productSale.product
-
-                            if(!groupedProductSales[name]) {
-                                groupedProductSales[name] = productSale.amount
-                            } else {
-                                groupedProductSales[name] += productSale.amount
-                            }
-                        }
-                    }
-
 
                     terminal.cyan(`MOST SOLD PRODUCT (total product sold): \n`)
-                    colorizeJSON(groupedProductSales)
+                    colorizeJSON(salesReport.mostSoldProduct)
 
                     terminal.once('key', async () => {
                         terminal.clear()
 
                         terminal.cyan(`REMAINING STOCKS: \n`)
-
-                        const stocks = await Manager.getStocks(selectedStoreId)
-                        const formatedStocks = stocks.map(s => ({
-                            amount: s.amount,
-                            name: s.product.name
-                        }))
-                        colorizeJSON(formatedStocks)
-
+                        colorizeJSON(salesReport.stocks)
 
                         terminal.once('key', () => {    
                             mainBusinessView()
