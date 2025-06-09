e640d19bc338fe2c2ba0059c97296cb4f6544a70
Pierre-Émile Brassard <pierreemilebrassard@gmail.com>
2025-06-08 20:58:27 -0400

    update test

--- test/controller.test.ts
+++ test/controller.test.ts
@@ -1,5 +1,7 @@
 import Manager from '../src/controller/manager'
 
+const shopId = 4
+
 describe('Product', () => {
     beforeAll(async () => {
         await Manager.addProduct("Melon", 5, "Fruit")
@@ -24,11 +26,11 @@ describe('Sales', () => {
         await Manager.createSales([{
             productId: 1,
             amount: 5
-        }], 4)
+        }], shopId)
     })
 
     it('Should find sales', async () => {
-        const res = await Manager.searchSales(undefined, 4)
+        const res = await Manager.searchSales(undefined, shopId)
         expect(res.length).toBeGreaterThan(0)
         expect(res[0].totalPrice).toBe(25)
         expect(res[0].productSales[0].amount).toBe(5)
@@ -42,13 +44,13 @@ describe('Orders', () => {
 
     it('Should find the order', async ()=> {
         const res = await Manager.getOrder()
-        expect(res.find(r => r.productId == 1 && r.shopId == 4 && r.amount == 5)).toBeTruthy()
+        expect(res.find(r => r.productId == 1 && r.shopId == shopId && r.amount == 5)).toBeTruthy()
     })
 
     
     it('Should delete the order', async ()=> {
-        await Manager.removeOrder(1, 5)
+        await Manager.removeOrder(1, shopId)
         const res = await Manager.getOrder()
-        expect(res.find(r => r.productId == 1 && r.shopId == 4 && r.amount == 5)).toBeFalsy()
+        expect(res.find(r => r.productId == 1 && r.shopId == shopId && r.amount == 5)).toBeFalsy()
     })
 })
