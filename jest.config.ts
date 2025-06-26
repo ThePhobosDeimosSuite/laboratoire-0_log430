ebe3ca83e3b88fcec03c763dc09a9896d0170bb5
Pierre-Émile Brassard <pierreemilebrassard@gmail.com>
2025-06-26 13:36:05 -0400

    update test for github action

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
-  verbose: true,
-  projects: [
-    '<rootDir>/src/module/product',
-    '<rootDir>/src/module/sales',
-  ],
+    verbose: true,
+    projects: ['<rootDir>/src/module/product', '<rootDir>/src/module/sales' ]
 };
 
 export default config;
--- src/module/sales/jest.config.ts
+++ src/module/sales/jest.config.ts
@@ -1,6 +1,7 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
+  displayName: 'sales',
   rootDir: '.',
   setupFiles: ['<rootDir>/jest.setup.js'],
   transform: {
